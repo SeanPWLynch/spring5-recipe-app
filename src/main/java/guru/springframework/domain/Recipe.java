db316bfd235a6e2b556839522ebccea3541ee2dd
Sean Lynch <sean.p.w.lynch@gmail.com>
2020-04-08 18:38:11 +0100

    added repositoes

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -27,8 +27,6 @@ public class Recipe {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
-
-
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
