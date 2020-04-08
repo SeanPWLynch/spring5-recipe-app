bd3e5305d5fa33d4bbc604a676131d2e89b4f62f
Sean Lynch <sean.p.w.lynch@gmail.com>
2020-04-08 18:17:23 +0100

    added unit of measure and relationship

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -14,9 +14,10 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    //private UnitOfMeasure uom;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
-    @ManyToOne()
+    @ManyToOne
     private Recipe recipe;
 
     public Long getId() {
@@ -49,5 +50,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
